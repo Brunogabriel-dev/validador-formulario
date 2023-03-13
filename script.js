1a5f0f0ebd7f59a5dbd1749ace12a20f9afc908b
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2023-03-13 11:15:18 -0300

    Inclusão de texto

--- script.js
+++ script.js
@@ -5,9 +5,28 @@ let B7Validator = {
 
     let inputs = form.querySelectorAll('input');
 
+    for(let i=0;i<inputs.length;i++) {
+      let input = inputs[i];
+      let check = B7Validator.checkInput(input);
+      if(check !== true) {
+        send = false;
+        // exibir o erro
+      }
+    }
+
+    send = false;
     if(send) {
       form.submit();
     }
+  },
+  checkInput:(input) => {
+    let rules = input.getAttribute('data-rules');
+
+    if(rules !== null) {
+
+    }
+
+    return true;
   }
 };
 
